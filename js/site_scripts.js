3eb5d813f747dc4926aaf9a0a517f91c62d9de92
VonBorstel-J <jordanvonborstel@gmail.com>
2022-06-19 22:02:03 -0700

    Everything except modal body will show up

--- js/site_scripts.js
+++ js/site_scripts.js
@@ -84,7 +84,7 @@ function generateCode(){
     
     //Activate function 
 
-    
+
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 
 // Activity page scripts
@@ -99,17 +99,15 @@ $(document).ready(function() {
         });
     });
     
-    $(document).ready(function(){
-        $('#activityModal').modal({
+    $(document).ready(function() {
+        var trText = 'doot doot'
+        $('#activityModal').modal( {
             keyboard: true,
             backdrop: "static",
             show:false,
-    
-        }).on('show', function(){ //subscribe to show method
-            $(function(){
-                $("#activityName").click(function(){
-                    var data1 = $(this).find("td:eq(0) input[type='text']").val();
-                    var data2 = $(this).find("td:eq(1) input[type='text']").val();
-                });
+            $('#activityText').bind('click', function(){ 
+                $(this).toggleClass('blue');
             });
+      });
     });
+ 
